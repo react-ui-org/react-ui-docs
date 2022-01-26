e21d3c895254357ea36a8b907cbbece53a82c93f
adamkudrna <adam@adamkudrna.cz>
2022-01-26 17:57:23 +0000

    Release new version

--- docs/component---src-pages-404-js-595bf20fc00b816e419e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[54],{w2l6:function(t,e,n){"use strict";n.r(e);n("mXGw");var i=n("aD51");e.default=()=>Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}]);
-//# sourceMappingURL=component---src-pages-404-js-595bf20fc00b816e419e.js.map
